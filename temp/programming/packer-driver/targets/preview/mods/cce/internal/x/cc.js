d76f8bbe8975effe3d1f9f0c3fa99c595d26ef2e
张鸣鸣 <>
2021-10-14 23:19:18 +0800

    qqqqq

---
+++ temp/programming/packer-driver/targets/preview/mods/cce/internal/x/cc.js
@@ -0,0 +1,113 @@
+System.register(["cce:/internal/x/cc-fu/base", "cce:/internal/x/cc-fu/gfx-webgl", "cce:/internal/x/cc-fu/gfx-webgl2", "cce:/internal/x/cc-fu/profiler", "cce:/internal/x/cc-fu/audio", "cce:/internal/x/cc-fu/tween", "cce:/internal/x/cc-fu/2d", "cce:/internal/x/cc-fu/ui", "cce:/internal/x/cc-fu/physics-2d-box2d", "cce:/internal/x/cc-fu/physics-2d-framework", "cce:/internal/x/cc-fu/intersection-2d", "cce:/internal/x/cc-fu/spine", "cce:/internal/x/cc-fu/dragon-bones"], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [function (_cceInternalXCcFuBase) {
+      var _exportObj = {};
+
+      for (var _key in _cceInternalXCcFuBase) {
+        if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _cceInternalXCcFuBase[_key];
+      }
+
+      _export(_exportObj);
+    }, function (_cceInternalXCcFuGfxWebgl) {
+      var _exportObj2 = {};
+
+      for (var _key2 in _cceInternalXCcFuGfxWebgl) {
+        if (_key2 !== "default" && _key2 !== "__esModule") _exportObj2[_key2] = _cceInternalXCcFuGfxWebgl[_key2];
+      }
+
+      _export(_exportObj2);
+    }, function (_cceInternalXCcFuGfxWebgl2) {
+      var _exportObj3 = {};
+
+      for (var _key3 in _cceInternalXCcFuGfxWebgl2) {
+        if (_key3 !== "default" && _key3 !== "__esModule") _exportObj3[_key3] = _cceInternalXCcFuGfxWebgl2[_key3];
+      }
+
+      _export(_exportObj3);
+    }, function (_cceInternalXCcFuProfiler) {
+      var _exportObj4 = {};
+
+      for (var _key4 in _cceInternalXCcFuProfiler) {
+        if (_key4 !== "default" && _key4 !== "__esModule") _exportObj4[_key4] = _cceInternalXCcFuProfiler[_key4];
+      }
+
+      _export(_exportObj4);
+    }, function (_cceInternalXCcFuAudio) {
+      var _exportObj5 = {};
+
+      for (var _key5 in _cceInternalXCcFuAudio) {
+        if (_key5 !== "default" && _key5 !== "__esModule") _exportObj5[_key5] = _cceInternalXCcFuAudio[_key5];
+      }
+
+      _export(_exportObj5);
+    }, function (_cceInternalXCcFuTween) {
+      var _exportObj6 = {};
+
+      for (var _key6 in _cceInternalXCcFuTween) {
+        if (_key6 !== "default" && _key6 !== "__esModule") _exportObj6[_key6] = _cceInternalXCcFuTween[_key6];
+      }
+
+      _export(_exportObj6);
+    }, function (_cceInternalXCcFu2d) {
+      var _exportObj7 = {};
+
+      for (var _key7 in _cceInternalXCcFu2d) {
+        if (_key7 !== "default" && _key7 !== "__esModule") _exportObj7[_key7] = _cceInternalXCcFu2d[_key7];
+      }
+
+      _export(_exportObj7);
+    }, function (_cceInternalXCcFuUi) {
+      var _exportObj8 = {};
+
+      for (var _key8 in _cceInternalXCcFuUi) {
+        if (_key8 !== "default" && _key8 !== "__esModule") _exportObj8[_key8] = _cceInternalXCcFuUi[_key8];
+      }
+
+      _export(_exportObj8);
+    }, function (_cceInternalXCcFuPhysics2dBox2d) {
+      var _exportObj9 = {};
+
+      for (var _key9 in _cceInternalXCcFuPhysics2dBox2d) {
+        if (_key9 !== "default" && _key9 !== "__esModule") _exportObj9[_key9] = _cceInternalXCcFuPhysics2dBox2d[_key9];
+      }
+
+      _export(_exportObj9);
+    }, function (_cceInternalXCcFuPhysics2dFramework) {
+      var _exportObj10 = {};
+
+      for (var _key10 in _cceInternalXCcFuPhysics2dFramework) {
+        if (_key10 !== "default" && _key10 !== "__esModule") _exportObj10[_key10] = _cceInternalXCcFuPhysics2dFramework[_key10];
+      }
+
+      _export(_exportObj10);
+    }, function (_cceInternalXCcFuIntersection2d) {
+      var _exportObj11 = {};
+
+      for (var _key11 in _cceInternalXCcFuIntersection2d) {
+        if (_key11 !== "default" && _key11 !== "__esModule") _exportObj11[_key11] = _cceInternalXCcFuIntersection2d[_key11];
+      }
+
+      _export(_exportObj11);
+    }, function (_cceInternalXCcFuSpine) {
+      var _exportObj12 = {};
+
+      for (var _key12 in _cceInternalXCcFuSpine) {
+        if (_key12 !== "default" && _key12 !== "__esModule") _exportObj12[_key12] = _cceInternalXCcFuSpine[_key12];
+      }
+
+      _export(_exportObj12);
+    }, function (_cceInternalXCcFuDragonBones) {
+      var _exportObj13 = {};
+
+      for (var _key13 in _cceInternalXCcFuDragonBones) {
+        if (_key13 !== "default" && _key13 !== "__esModule") _exportObj13[_key13] = _cceInternalXCcFuDragonBones[_key13];
+      }
+
+      _export(_exportObj13);
+    }],
+    execute: function () {}
+  };
+});
+//# sourceMappingURL=cc.js.map
